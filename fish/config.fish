b08b10d1a9a16c251b698128841d7639a3eedada
Erwan <erwan.simonetti@epitech.eu>
2021-10-28 14:40:11 +0200

    add(fish/vscode): first config files

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set fish_greeting
+abbr -a c clear
+abbr -a glo git log --pretty=oneline
+abbr -a gca git commit --amend
+abbr -a gst git status -s
+abbr -a music open https://www.deezer.com/fr/
