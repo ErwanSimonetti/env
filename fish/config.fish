06a84cddb2beabc6287c9b540a2b20ccffe29dc5
Erwan <erwan.simonetti@epitech.eu>
2021-11-02 23:43:41 +0100

    feat(fish/vscode): various updates including fish prompt

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,4 @@ abbr -a glo git log --pretty=oneline
 abbr -a gca git commit --amend
 abbr -a gst git status -s
 abbr -a music open https://www.deezer.com/fr/
+set -gx PATH /home/erwansimonetti/.local/bin:/home/erwansimonetti/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin
