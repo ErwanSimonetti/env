378cb5b35497a107af86bc41585599e718e42607
Erwan <erwan.simonetti@epitech.eu>
2021-11-18 16:29:16 +0100

    feat(fish): updated abbreviations

--- fish/config.fish
+++ fish/config.fish
@@ -7,4 +7,8 @@ abbr -a glo git log --pretty=oneline
 abbr -a gca git commit --amend
 abbr -a gst git status -s
 abbr -a music open https://www.deezer.com/fr/
+abbr -a youtube open https://www.youtube.com/
+abbr -a bashrc code ~/.config/fish/config.fish
+abbr -a prompt code ~/.config/fish/functions/fish_prompt.fish
+abbr -a right_prompt code ~/.config/fish/functions/fish_right_prompt.fish
 set -gx PATH /home/erwansimonetti/.local/bin:/home/erwansimonetti/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin
