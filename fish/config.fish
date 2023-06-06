af38537f2e74c163e436e5816dec86f5b0105952
Erwan <erwan.simonetti@epitech.eu>
2023-06-06 23:54:51 +0200

    feat(fish): updated path variables

--- fish/config.fish
+++ fish/config.fish
@@ -11,6 +11,5 @@ abbr -a youtube open https://www.youtube.com/
 abbr -a bashrc code ~/.config/fish/config.fish
 abbr -a prompt code ~/.config/fish/functions/fish_prompt.fish
 abbr -a right_prompt code ~/.config/fish/functions/fish_right_prompt.fish
-abbr -a engine cd /home/erwansimonetti/Mergify/mergify-engine/
-abbr -a dashboard cd /home/erwansimonetti/Mergify/mergify.io/
-set -gx PATH /home/erwansimonetti/.local/bin:/home/erwansimonetti/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin /var/lib/snapd/snap/bin/
+abbr -a epitech cd /home/erwansimonetti/Epitech/Tek3/
+set -gx PATH /home/erwansimonetti/.local/bin:/home/erwansimonetti/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin /var/lib/snapd/snap/bin/ /home/erwansimonetti/Epitech/flutter/bin /home/erwansimonetti/Downloads/apache-maven-3.8.7/bin
