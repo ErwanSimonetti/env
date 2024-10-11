e3598b09d148ffbbb2cb36ade21c2f57a38c52b7
Erwan <erwan.simonetti@epitech.eu>
2024-10-11 14:02:17 +0200

    feat(fish): new variables and config

--- fish/config.fish
+++ fish/config.fish
@@ -12,4 +12,10 @@ abbr -a bashrc code ~/.config/fish/config.fish
 abbr -a prompt code ~/.config/fish/functions/fish_prompt.fish
 abbr -a right_prompt code ~/.config/fish/functions/fish_right_prompt.fish
 abbr -a epitech cd /home/erwansimonetti/Epitech/Tek3/
-set -gx PATH /home/erwansimonetti/.local/bin:/home/erwansimonetti/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin /var/lib/snapd/snap/bin/ /home/erwansimonetti/Epitech/flutter/bin /home/erwansimonetti/Downloads/apache-maven-3.8.7/bin
+set -gx PATH /home/erwansimonetti/.local/bin:/home/erwansimonetti/bin:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin /var/lib/snapd/snap/bin/ /home/erwansimonetti/Epitech/flutter/bin /home/erwansimonetti/Downloads/apache-maven-3.8.7/bin /home/erwansimonetti/Keimyung/vcpkg
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /usr/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
